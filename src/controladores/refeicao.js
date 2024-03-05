109030cb6a1ee0088544b9f3d9b06dd128c957ec
mauricio alejandro <msuberoj11@gmail.com>
2024-03-04 21:32:17 -0300

    alteracoes em exibir refeicoes do dia

--- src/controladores/refeicao.js
+++ src/controladores/refeicao.js
@@ -129,33 +129,44 @@ const exibirRefeicoesDoDia = async (req, res) => {
   const { dia_da_refeicao } = req.query;
   const usuarioId = req.usuario.id;
 
+  if (!dia_da_refeicao) {
+    return res.status(400).json({message: "É necesario informar algun dia de refeição"})
+  }
+
   try {
-    if (dia_da_refeicao) {
-      let resultado = [];
+      let resultado 
+      let objetoDoResultado = []
 
       for (let dia of dia_da_refeicao) {
+
+
         const idDasRefeicoes = await pool.query(
           "select * from refeicoes_do_dia where usuario_id = $1 and dia_da_refeicao ilike $2",
           [usuarioId, dia]
         );
 
-        const refeicoes = await pool.query(
-          "select * from refeicoes where id = $1",
-          [idDasRefeicoes.rows[0]]
-        );
-
         if (idDasRefeicoes.rowCount < 1) {
-          resultado = resultado;
-        } else {
-          resultado.push({
-            nome_refeicao: refeicoes.rows[0].nome_refeicao,
-            dia_da_refeicao: idDasRefeicoes.rows[0].dia_da_refeicao
-          });
-        }
-      }
+          objetoDoResultado.push({ dia_da_refeicao: dia , refeicoes: [] })
+          console.log(objetoDoResultado);
+          console.log("dia 1", dia);
+        }else{
 
+          for (let objeto of idDasRefeicoes.rows) {
+  
+            const refeicoes = await pool.query(
+              "select * from refeicoes where id = $1",
+              [objeto.refeicao_id]
+            );
+            objetoDoResultado.push({ dia_da_refeicao: dia , refeicoes: refeicoes.rows })
+            console.log(objetoDoResultado);
+            console.log("dia", dia);
+          }
+        }
+
+     
+        resultado = objetoDoResultado
+      }
       return res.status(200).json(resultado);
-    }
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error.message });
