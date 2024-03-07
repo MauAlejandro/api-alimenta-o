f7e33ebae72eeef77fadd81ac196d7b426512402
mauricio alejandro <msuberoj11@gmail.com>
2024-03-07 17:12:50 -0300

    exibir refeicoes terminado

--- src/controladores/refeicao.js
+++ src/controladores/refeicao.js
@@ -130,43 +130,53 @@ const exibirRefeicoesDoDia = async (req, res) => {
   const usuarioId = req.usuario.id;
 
   if (!dia_da_refeicao) {
-    return res.status(400).json({message: "É necesario informar algun dia de refeição"})
+    return res
+      .status(400)
+      .json({ message: "É necesario informar algun dia de refeição" });
   }
 
   try {
-      let resultado 
-      let objetoDoResultado = []
+    let resultado;
+    let objetoDoResultado = [];
 
-      for (let dia of dia_da_refeicao) {
+    for (let dia of dia_da_refeicao) {
+      const idDasRefeicoes = await pool.query(
+        "select * from refeicoes_do_dia where usuario_id = $1 and dia_da_refeicao ilike $2",
+        [usuarioId, dia]
+      );
 
+      if (idDasRefeicoes.rowCount < 1) {
+        objetoDoResultado.push({ dia_da_refeicao: dia, refeicoes: [] });
+      } else {
+        for (let objeto of idDasRefeicoes.rows) {
+          const refeicoes = await pool.query(
+            "select * from refeicoes where id = $1",
+            [objeto.refeicao_id]
+          );
 
-        const idDasRefeicoes = await pool.query(
-          "select * from refeicoes_do_dia where usuario_id = $1 and dia_da_refeicao ilike $2",
-          [usuarioId, dia]
-        );
+          let mesmoDia = false;
+          let indiceAtual = objetoDoResultado.length - 1;
 
-        if (idDasRefeicoes.rowCount < 1) {
-          objetoDoResultado.push({ dia_da_refeicao: dia , refeicoes: [] })
-          console.log(objetoDoResultado);
-          console.log("dia 1", dia);
-        }else{
+          if (objetoDoResultado.length > 0) {
+            objetoDoResultado[indiceAtual].dia_da_refeicao == dia
+              ? (mesmoDia = true)
+              : (mesmoDia = false);
+          }
 
-          for (let objeto of idDasRefeicoes.rows) {
-  
-            const refeicoes = await pool.query(
-              "select * from refeicoes where id = $1",
-              [objeto.refeicao_id]
-            );
-            objetoDoResultado.push({ dia_da_refeicao: dia , refeicoes: refeicoes.rows })
-            console.log(objetoDoResultado);
-            console.log("dia", dia);
+          if (mesmoDia) {
+            objetoDoResultado[indiceAtual].refeicoes.push(refeicoes.rows[0]);
+          } else {
+            objetoDoResultado.push({
+              dia_da_refeicao: dia,
+              refeicoes: [refeicoes.rows[0]],
+            });
           }
         }
-
-     
-        resultado = objetoDoResultado
       }
-      return res.status(200).json(resultado);
+
+      resultado = objetoDoResultado;
+    }
+    return res.status(200).json(resultado);
   } catch (error) {
     console.log(error);
     return res.status(500).json({ error: error.message });
